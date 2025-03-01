fa0453e818ae4676ed84b57499745e3dbbf5fa88
rukkiechovwe <rukkiechowe@gmail.com>
2025-03-01 07:38:55 +0100

    fix: update income page ui

--- src/app/(create-budget)/budgets/new/income/[id]/page.tsx
+++ src/app/(create-budget)/budgets/new/income/[id]/page.tsx
@@ -92,40 +92,47 @@ const Page = ({ params }: { params: { id: string } }) => {
       transition={{ duration: 0.3 }}
       className=" w-[100vw] relative min-h-[100vh] max-w-[500px] bg-white"
     >
-      <Header link='/budgets' title="Create new budget" />
+      <Header link="/budgets" title="Create new budget" />
       <ToastContainer />
-      <div className='mt-[39.5px] mb-[140px] px-[24px] w-full'>
-        <div className='flex gap-[8px] justify-between items-center'>
-          <p className='mt-[24px] mb-[16px] font-[500] text-[20px]'>Set your income</p>
-          <div className='relative h-[42px] w-[42px]'>
+
+      <div className="mt-6 mb-[140px] px-[24px] w-full">
+        <div className="flex gap-[8px] justify-between items-center">
+          <p className="mt-[24px] mb-[16px] font-[500] text-[20px]">Set your income</p>
+          <div className="relative h-[42px] w-[42px]">
             <div
-              className='absolute inset-0 rounded-full'
+              className="absolute inset-0 rounded-full"
               style={{
                 background: 'conic-gradient(#66C227 0% 50%, #EFEFF0 50% 100%)',
               }}
             />
-            <div className='absolute inset-[6px] bg-white rounded-full flex items-center justify-center'>
-              <span className='text-[#2D2D2D] text-[12px] font-[700]'>1/2</span>
+            <div className="absolute inset-[6px] bg-white rounded-full flex items-center justify-center">
+              <span className="text-[#2D2D2D] text-[12px] font-[700]">1/2</span>
             </div>
           </div>
         </div>
 
-        <div className='bg-[#F7F7F9] rounded-[20px] mb-5'>
-          <h1 className='text-[#575757] leading-[24px] p-4'>Income</h1>
-          <hr className='fill-white' />
-          <div className='grid grid-cols-1 max-h-[calc(65vh-140px)] px-4 pt-2 pb-3 space-y-3'>
-            <div className='overflow-y-auto overflow-x-hidden  max-h-[calc(65vh-140px)] '>
+        <div className="bg-[#F7F7F9] rounded-[20px] mb-5 border border-[#EFEFF0]">
+          <h1 className="text-[#575757] leading-[24px] p-4 bg-white rounded-t-[20px]">
+            Income Streams
+          </h1>
+          <hr className="fill-white" />
+          <div className="grid grid-cols-1 max-h-[calc(65vh-140px)] px-4 pt-2 pb-3 space-y-3">
+            <div className="overflow-y-auto overflow-x-hidden  max-h-[calc(65vh-140px)] ">
               {allIncomes.map((income, index) => (
-                  <form
-                      key={index} className="flex z-[100] relative  transition-all ease-in mt-[8px] items-center justify-between w-full gap-[8px]">
-                  <div className='flex items-center gap-3 w-full'>
-                    <Image src={moneyIcon} className='size-8' alt={'icon'} />
-                    <div className='text-[#514F6E] min-w-[100px] w-[80px] text-[14px] font-[500] inline-block'>
+                <form
+                  key={index}
+                  className="flex z-[100] relative  transition-all ease-in mt-[8px] items-center justify-between w-full gap-[8px]"
+                >
+                  <div className="flex items-center gap-3 w-full">
+                    <Image src={moneyIcon} className="size-8" alt={'icon'} />
+                    <div className="text-[#514F6E] min-w-[140px] w-[80px] text-[14px] font-[500] inline-block">
                       <input
-                        className='bg-transparent outline-[#66C227] px-2 w-full text-ellipsis overflow-hidden whitespace-nowrap'
+                        className="bg-transparent outline-[#66C227] px-2 w-full text-ellipsis overflow-hidden whitespace-nowrap"
                         type="text"
-                        placeholder='Enter income'
-                        onChange={(e) => handleIncomeChange(index, 'name', e.target.value)}
+                        placeholder="Enter income name"
+                        onChange={(e) =>
+                          handleIncomeChange(index, 'name', e.target.value)
+                        }
                         ref={(el) => {
                           inputRefs.current[index] = el;
                         }}
@@ -133,13 +140,19 @@ const Page = ({ params }: { params: { id: string } }) => {
                       />
                     </div>
                   </div>
-                  <div className='flex items-start'>
-                    <div className='bg-white rounded-[8px] py-[4px] px-[8px] items-center flex  gap-[0px]'>
+                  <div className="flex items-start">
+                    <div className="bg-white rounded-[8px] py-[4px] px-[8px] items-center flex  gap-[0px]">
                       <h2>₦</h2>
                       <div className="relative inline-block w-full">
                         <input
                           value={formatNumber(income.amount)}
-                          onChange={(e) => handleIncomeChange(index, 'amount', parseNumber(e.target.value))}
+                          onChange={(e) =>
+                            handleIncomeChange(
+                              index,
+                              'amount',
+                              parseNumber(e.target.value),
+                            )
+                          }
                           type="text"
                           className="px-2 py-1 rounded focus:outline-none border-none focus:border-none transition-all duration-200"
                           style={{ width: getInputWidth(index), maxWidth: '140px' }}
@@ -158,21 +171,30 @@ const Page = ({ params }: { params: { id: string } }) => {
                 </form>
               ))}
             </div>
-            <button onClick={handleAddIncome} className='flex gap-x-4 items-center hover:scale-105'>
-              <Image src={addAnotherIcon} className='size-8' alt={'icon'} />
-              <span className='text-[#514F6E] text-[14px] font-[500]'>Add New</span>
+            <button
+              onClick={handleAddIncome}
+              className="flex gap-x-4 items-center hover:scale-105"
+            >
+              <Image src={addAnotherIcon} className="size-8" alt={'icon'} />
+              <span className="text-[#514F6E] text-[14px] font-[500]">Add New</span>
             </button>
           </div>
         </div>
       </div>
 
-      <div className='p-[24px] max-w-[500px] mt-[12rem] fixed  z-10 bg-[#ffffffaa] backdrop-blur-lg bottom-0 w-[100%] border-t-[2px] border-t-[#EFF0F6]'>
-        <div className='w-full flex items-center gap-4'>
-          <button onClick={() => navigate.push(`/budgets/`)} className="btn w-full rounded-[32px] px-[28px] py-[14px] bg-[#E7E7EA] text-black flex items-center justify-center gap-[8px] font-[500]">
+      <div className="p-[24px] max-w-[500px] mt-[12rem] fixed  z-10 bg-[#ffffffaa] backdrop-blur-lg bottom-0 w-[100%] border-t-[2px] border-t-[#EFF0F6]">
+        <div className="w-full flex items-center gap-4">
+          <button
+            onClick={() => navigate.push(`/budgets/`)}
+            className="btn w-full rounded-[32px] px-[28px] py-[14px] bg-[#E7E7EA] text-black flex items-center justify-center gap-[8px] font-[500]"
+          >
             <BsArrowLeft />
             Previous
           </button>
-          <button onClick={() => handleSubmit()} className="btn w-full rounded-[32px] px-[28px] py-[14px] bg-black text-[#FAFAFA] flex items-center justify-center gap-[8px] font-[500]">
+          <button
+            onClick={() => handleSubmit()}
+            className="btn w-full rounded-[32px] px-[28px] py-[14px] bg-black text-[#FAFAFA] flex items-center justify-center gap-[8px] font-[500]"
+          >
             Proceed <BsArrowRight />
           </button>
         </div>
