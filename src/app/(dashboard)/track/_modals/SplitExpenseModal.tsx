a1cf1a6ed4a6661f195d03eb5ced9c1c431c7e92
rukkiechovwe <rukkiechowe@gmail.com>
2025-03-18 10:01:53 +0100

    fix: .

--- src/app/(dashboard)/track/_modals/SplitExpenseModal.tsx
+++ src/app/(dashboard)/track/_modals/SplitExpenseModal.tsx
@@ -87,7 +87,7 @@ export default function SplitExpenseModal({
           return prev;
         });
 
-        const newTotal = otherCategoriesTotal + value;
+        const newTotal = otherCategoriesTotal + value;        
         setAmountDetails((prev) => ({
           ...prev,
           amountLeft: Number((amountToSplit - newTotal).toFixed(2)),
@@ -259,11 +259,11 @@ export default function SplitExpenseModal({
                         type="text"
                         inputMode="decimal"
                         className="w-full text-sm bg-transparent focus:outline-none"
-                        value={formatCategoryNumber(
+                        value={
                           splitCategories
                             .find((item) => item.allocationId === category.uid)
-                            ?.amount?.toString() || '',
-                        )}
+                            ?.amount?.toString() || ''
+                        }
                         placeholder="0.00"
                         onChange={(e) => {
                           // Only allow numbers and one decimal point
